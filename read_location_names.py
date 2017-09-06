94ab77a9b4bf1d2f06e27e871db80de9219d86bf
Eric <innovacious@hotmail.com>
2017-09-07 01:16:19 +0800

    got generation of augmented corpus right.

--- read_location_names.py
+++ read_location_names.py
@@ -2,7 +2,7 @@ import re
 
 
 def read_location_names():
-    locations = []
+    locations_without_suffix = []
     locations_with_suffix = []
     location_suffix_pattern = r'省'
     # location_suffix_pattern = r'(省)|(市)|(区)|(县)|(自治县)|(自治州)'
@@ -11,21 +11,26 @@ def read_location_names():
             if re.match(r'\d+\.', line) is not None:
                 province = line.split('.')[1].split()[0]
                 locations_with_suffix.append(province)
-                province = re.sub(location_suffix_pattern, '', province)
-                locations.append(province)
+                province_without_suffix = re.sub(location_suffix_pattern, '', province)
+                if province_without_suffix != province:
+                    locations_without_suffix.append(province_without_suffix)
             elif re.match(r'\d{4}.+。', line) is not None:
                 continue
             else:
                 locations_inline = line.split()
                 locations_with_suffix += locations_inline
                 for location in locations_inline:
-                    location = re.sub(location_suffix_pattern, '', location)
-                    locations.append(location)
+                    location_without_suffix = re.sub(location_suffix_pattern, '', location)
+                    if location_without_suffix != location:
+                        locations_without_suffix.append(location_without_suffix)
 
     # with open('./data/location_name_data.txt', 'w', encoding='utf8') as f:
     #     for location in locations:
     # returns unsegmented strings
-    return locations, locations_with_suffix
 
-read_location_names()
+    # mutually exclusive
+    return locations_with_suffix, locations_without_suffix
+
+# w, wt = read_location_names()
+# pass
 # print(len(locations))
